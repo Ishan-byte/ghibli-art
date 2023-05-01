d7acc3bbab17e22ea99d9bcbb083f24bba33ea38
Ishan-byte <limbuisan@gmail.com>
2023-05-01 14:12:48 +0545

    Creates and Integrates Fiber Container in the Application

---
+++ src/components/FiberContainer.js
@@ -0,0 +1,15 @@
+// Imports
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+
+// Container or Space for displaying 3D Model Objects
+// I have specified the Camera positiong for the Container as well as enabled Shadow rendering.
+const FiberContainer = () => {
+  return (
+    <Canvas camera={{ position: [14.4666, 2.0365, 5.556165], fov: 40 }} shadows>
+      <OrbitControls minDistance={1} maxDistance={200} />
+    </Canvas>
+  );
+};
+
+export default FiberContainer;
