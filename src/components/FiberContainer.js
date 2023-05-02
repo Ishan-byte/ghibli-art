7d037267b824ca646d45e80f8373f964f680416c
Ishan-byte <limbuisan@gmail.com>
2023-05-02 16:28:22 +0545

    Test

--- src/components/FiberContainer.js
+++ src/components/FiberContainer.js
@@ -6,7 +6,7 @@ import { OrbitControls } from "@react-three/drei";
 // I have specified the Camera positiong for the Container as well as enabled Shadow rendering.
 const FiberContainer = () => {
   return (
-    <Canvas camera={{ position: [14.4666, 2.0365, 5.556165], fov: 40 }} shadows>
+    <Canvas camera={{ position: [14.4666, 2.0365, 5.556165], fov: 40 }}>
       <OrbitControls minDistance={1} maxDistance={200} />
     </Canvas>
   );
---
+++ src/components/Scene.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Scene = () => {
+  return (
+    <>
+      <ambientLight />
+      <
+    </>
+  );
+};
+
+export default Scene;
